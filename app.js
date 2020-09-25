7cd5badb794ee09e2828f52a363ef7da8c80d177
Nicholas Konzen <nicholaskonzen@gmail.com>
2020-09-25 16:02:55 -0700

    Programmed sort employees by department and reformatted display all employees

--- app.js
+++ app.js
@@ -11,24 +11,46 @@ const connection = mysql.createConnection({
     database: 'employee_db'
 });
 
-connection.connect((err, res) => {
-    if (err) throw err;
-    console.log(`Connection made at id ${connection.threadId}`)
-});
-
-const initOptions = ['View All Employees', 'View All Roles', 'View All Departments'];
+const initOptions = ['View All Employees', 'View All Roles', 'View All Departments', 'View All Employees by Department'];
 
 console.log(wordart);
 
-function mainRoute(route) {
+function testConnection() {
     return new Promise((res, rej) => {
+        connection.connect((err, response) => {
+            if (err) throw err;
+            console.log(`Connection made at id ${connection.threadId}`)
+            res();
+        });
+    })
+}
+
+async function mainRoute(route) {
+    return new Promise(async (res, rej) => {
         if (route.value === initOptions[0]) {
-            connection.query('SELECT * FROM employees', (err, result) => {
-                if (err) throw err;
-                console.log('')
-                console.table(result)
-                res();
-            });
+            connection.query(`
+            SELECT
+	            e.employee_id "ID",
+	            e.first_name "First Name",
+                e.last_name "Last Name",
+                e.manager_id "Manager ID",
+                r.title "Role",
+                d.name "Department"
+            FROM
+	            employees e,
+	            roles r,
+	            departments d
+            WHERE 
+            	e.role_id = r.role_id
+                AND r.department_id = d.department_id
+            ORDER BY e.employee_id;
+            `,
+                (err, result) => {
+                    if (err) throw err;
+                    console.log('')
+                    console.table(result)
+                    res();
+                });
         } else if (route.value === initOptions[1]) {
             connection.query('SELECT * FROM roles', (err, result) => {
                 if (err) throw err;
@@ -44,22 +66,83 @@ function mainRoute(route) {
                 console.log('')
                 console.table(result)
                 res();
+
+                // main();
             });
+        } else if (route.value === initOptions[3]) {
+            const departments = await getDepartments();
+            const departmentsArray = []
+
+            console.log('');
+            departments.forEach(val => {
+                departmentsArray.push(val.name)
+            })
+
+            const department = await inquirer.prompt({
+                name: 'value',
+                type: 'list',
+                choices: departmentsArray,
+                message: 'Select a Department'
+            })
+            console.table(await getByDepartment(department.value));
+
+            // res();
         }
     })
 };
 
-async function main() {
-    const where = await inquirer.prompt({
-        name: 'value',
-        type: 'list',
-        choices: initOptions,
-        message: 'What would you like to do?'
-    })
+function getDepartments() {
+    return new Promise((res, rej) => {
+        connection.query('SELECT * FROM departments', (err, result) => {
+            if (err) throw err;
 
-    await mainRoute(where);
+            res(result);
+        })
+    })
+}
+
+function getByDepartment(department) {
+    return new Promise((res, rej) => {
+        connection.query(`
+        SELECT 
+	        e.employee_id "Employee ID", 
+	        e.first_name "First Name", 
+            e.last_name "Last Name", 
+            e.manager_id "Manager ID", 
+            r.title "Role", 
+            d.name "Department" 
+        FROM 
+	        employees e, 
+            departments d, 
+            roles r
+        WHERE 
+	        r.role_id = e.role_id 
+            AND r.department_id = d.department_id 
+            AND d.name = ?`, [department], (err, result) => {
+            if (err) throw err;
+
+            res(result);
+        })
+    })
+}
+
+async function init() {
+    await testConnection();
+
+    async function main() {
+        const where = await inquirer.prompt({
+            name: 'value',
+            type: 'list',
+            choices: initOptions,
+            message: 'What would you like to do?'
+        })
+
+        await mainRoute(where);
+
+        main();
+    }
 
     main();
 }
 
-main();
+init();
